b6bbb331baf1e15f3a8705c67eb91090a0c7896d
alex1779 <alex1779@gmail.com>
2023-09-20 17:18:55 -0300

    Adding new component to main app

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Calculator from './components/Calculator';
+import Quotes from './components/Quotes';
+
+function App() {
+  return (
+    <section className="mySection">
+      <Calculator />
+      <Quotes />
+    </section>
+
+  );
+}
+
+export default App;
