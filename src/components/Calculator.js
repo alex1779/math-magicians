4adb10f249e5b5208c0c8cec46d3fb90acd760a2
alex1779 <alex1779@gmail.com>
2023-09-27 05:51:58 -0300

    Modification according to review on style, pages home, calculator, and quotes

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -30,40 +30,45 @@ function Calculator() {
   };
 
   return (
+    <>
+      <div className="intro">
 
-    <div className="default">
+        <p className="intro-p">Let&apos;s do some math!</p>
 
-      <input type="text" placeholder="0" className="input-result" value={displayNumber} />
+        <div className="default">
+          <input type="text" placeholder="0" className="input-result" value={displayNumber} />
+          <div className="buttons">
 
-      <div className="buttons">
+            <ButtonGray string="AC" clickCalc={clickCalc} />
+            <ButtonGray string="+/-" clickCalc={clickCalc} />
+            <ButtonGray string="%" clickCalc={clickCalc} />
+            <ButtonOrange string="÷" clickCalc={clickCalc} />
 
-        <ButtonGray string="AC" clickCalc={clickCalc} />
-        <ButtonGray string="+/-" clickCalc={clickCalc} />
-        <ButtonGray string="%" clickCalc={clickCalc} />
-        <ButtonOrange string="÷" clickCalc={clickCalc} />
+            <ButtonGray string="7" clickCalc={clickCalc} />
+            <ButtonGray string="8" clickCalc={clickCalc} />
+            <ButtonGray string="9" clickCalc={clickCalc} />
+            <ButtonOrange string="x" clickCalc={clickCalc} />
 
-        <ButtonGray string="7" clickCalc={clickCalc} />
-        <ButtonGray string="8" clickCalc={clickCalc} />
-        <ButtonGray string="9" clickCalc={clickCalc} />
-        <ButtonOrange string="x" clickCalc={clickCalc} />
+            <ButtonGray string="4" clickCalc={clickCalc} />
+            <ButtonGray string="5" clickCalc={clickCalc} />
+            <ButtonGray string="6" clickCalc={clickCalc} />
+            <ButtonOrange string="-" clickCalc={clickCalc} />
 
-        <ButtonGray string="4" clickCalc={clickCalc} />
-        <ButtonGray string="5" clickCalc={clickCalc} />
-        <ButtonGray string="6" clickCalc={clickCalc} />
-        <ButtonOrange string="-" clickCalc={clickCalc} />
+            <ButtonGray string="1" clickCalc={clickCalc} />
+            <ButtonGray string="2" clickCalc={clickCalc} />
+            <ButtonGray string="3" clickCalc={clickCalc} />
+            <ButtonOrange string="+" clickCalc={clickCalc} />
 
-        <ButtonGray string="1" clickCalc={clickCalc} />
-        <ButtonGray string="2" clickCalc={clickCalc} />
-        <ButtonGray string="3" clickCalc={clickCalc} />
-        <ButtonOrange string="+" clickCalc={clickCalc} />
+            <ButtonZero string="0" clickCalc={clickCalc} />
+            <ButtonGray string="." clickCalc={clickCalc} />
+            <ButtonOrange string="=" clickCalc={clickCalc} />
 
-        <ButtonZero string="0" clickCalc={clickCalc} />
-        <ButtonGray string="." clickCalc={clickCalc} />
-        <ButtonOrange string="=" clickCalc={clickCalc} />
+          </div>
+        </div>
 
       </div>
 
-    </div>
+    </>
 
   );
 }
--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,39 +1,13 @@
-import { useState, useEffect } from 'react';
-
 function Quotes() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch('https://api.api-ninjas.com/v1/quotes?limit=1&category=money', {
-        headers: {
-          'X-Api-Key': '417MZ7DAIIDohifAq1HR0g==C8ex0UAh8uWfaqqG',
-        },
-      });
-      const json = await res.json();
-      setData(json);
-    };
-    fetchData();
-  }, [setData]);
-
   return (
-    <ul>
-      {data.map((item) => (
-        <li key={item.id}>
-          <strong>Author:</strong>
-          {' '}
-          {item.author}
-          {' '}
-          <strong>Category:</strong>
-          {' '}
-          {item.category}
-          {' '}
-          <strong>Quote:</strong>
-          {' '}
-          {item.quote}
-        </li>
-      ))}
-    </ul>
+    <>
+      <p style={{
+        fontSize: 20, color: 'black', paddingTop: '10vw', paddingLeft: '10vw',
+      }}
+      >
+        Pure mathematics is, in its way, the poetry of logical ideas.— Albert Einstein.
+      </p>
+    </>
   );
 }
 
