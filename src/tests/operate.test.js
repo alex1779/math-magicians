037d0f043ee2a1160f38fbcb8fbbb5a8c3874994
alex1779 <alex1779@gmail.com>
2023-09-30 00:00:05 -0300

    test all the functionalities & scenarios of operate.js

--- src/tests/operate.test.js
+++ src/tests/operate.test.js
@@ -1,9 +1,24 @@
 import operate from '../logic/operate';
 
-const numberOne = 2;
-const numberTwo = 4;
-const operation = 'x';
+const numberOne = 4;
+const numberTwo = 2;
+
+test('return the result of the sum of two numbers', () => {
+  const operation = '+';
+  expect(operate(numberOne, numberTwo, operation)).toEqual('6');
+});
+
+test('return the result of the subtraction of two numbers', () => {
+  const operation = '-';
+  expect(operate(numberOne, numberTwo, operation)).toEqual('2');
+});
 
 test('return the result of multiplication of two numbers', () => {
+  const operation = 'x';
   expect(operate(numberOne, numberTwo, operation)).toEqual('8');
+});
+
+test('return the result of the division of two numbers', () => {
+  const operation = '÷';
+  expect(operate(numberOne, numberTwo, operation)).toEqual('2');
 });
