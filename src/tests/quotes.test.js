bd8fc833dc63f74a7f71218880e0a1ade1a51344
Esteban <cepalacios@outlook.com>
2023-09-30 11:17:54 -0500

    Implement Quotes test with mock response
    Installed msw to create a mock response
    Created a mock.js file with the mock response using msw, including the id
    Implemented the test using msw/node to mock the response modeled with
    mock.js
    Used async and await to wait for the component to fully render and for
    the mock response to finish

--- src/tests/quotes.test.js
+++ src/tests/quotes.test.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import { waitFor, render } from '@testing-library/react';
+import { setupServer } from 'msw/node';
 import Quotes from '../components/Quotes';
+import handlers from './mocks';
+
+const server = setupServer(...handlers); // Create a server with your handlers
+
+beforeAll(() => server.listen()); // Start the server before running tests
+afterEach(() => server.resetHandlers()); // Reset request handlers between tests
+afterAll(() => server.close()); // Close the server after all tests
 
 test('Confirm that Quotes renders correctly', async () => {
-  const quote = {
-    quote: 'Money is the root of all evil.',
-    author: 'Mark Twain',
-    categories: 'money',
-  };
-  const { container } = render(<Quotes quote={quote} />);
-  console.log(`this is the container: ${container}`);
+  const { container } = render(<Quotes />);
 
   await waitFor(() => {
     // Check that the li element is rendered
     const liElement = container.querySelector('li');
-    console.log(`this is the li element: ${liElement}`);
     expect(liElement).toBeInTheDocument();
   });
 
